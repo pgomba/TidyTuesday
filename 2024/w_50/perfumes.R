eeba8a4ff8f34c3d4b21b22ace58cc23b17083d4
Pablo Gomez Barreiro <pablogomezbr@hotmail.es>
2024-12-10 21:26:36 +0000

    update

---
+++ 2024/w_50/perfumes.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(viridis)
+library(showtext)
+
+showtext_auto()
+font_add_google("Roboto", "roboto")
+
+parfumo_data_clean <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024-12-10/parfumo_data_clean.csv')
+
+perfume<-parfumo_data_clean%>%
+  drop_na(Main_Accords)%>%
+  drop_na(Release_Year)
+
+test<-perfume %>%
+  mutate(Main_Accords= gsub(",","",Main_Accords))%>%
+  separate_rows(.,Main_Accords, sep = "\\s+") %>% # Split words into separate rows
+  group_by(Release_Year, Main_Accords) %>%             # Group by year and word
+  summarise(frequency = n(), .groups = "drop") %>% # Count occurrences
+  arrange(Release_Year, desc(frequency))%>%
+  filter(Release_Year>=2000)%>%
+  filter(Main_Accords!="Chypre")
+
+
+ggplot(test,aes(x=Release_Year,y=frequency))+
+  geom_col(aes(fill=frequency),colour="black")+
+  scale_fill_viridis(breaks = c(0, 200,500,800),option = "turbo")+
+  facet_wrap(Main_Accords~.,
+             strip.position = "bottom")+
+  scale_x_continuous(breaks = seq(2000,2024,4))+
+  theme_void()+
+  theme(plot.background =  element_rect(fill="#191919"),
+        text = element_text(family = "roboto",colour="white",size=60),
+        legend.direction = "horizontal",
+        legend.position = "bottom",
+        axis.text.x = element_text(angle=90,colour="white")
+        
+        )+
+  labs(fill="Frequency",
+       subtitle = " ",
+       title = "Fragrance Accord Release Trends (2000–2024)",
+       caption = "#tidyTuesday |week 50, 2024 | @pagomba")
+
+ggsave("perfumes.png",dpi="retina",width = 16.9,height = 9.53)  
--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # #TidyTuesday
-# 2021
-## Week 31 - Olympic Medals [(code)](https://github.com/pgomba/TidyTuesday/tree/main/2021/w31_Olimp)
-![Olimpic](2021/w31_Olimp/plots/spain_medals.jpeg)
-other countries plots [(here)](https://github.com/pgomba/TidyTuesday/tree/main/2021/w31_Olimp/plots)
-## Week 32 - Paralympic Medals [(code)](https://github.com/pgomba/TidyTuesday/tree/main/2021/w32_Para)
-![MvsW](2021/w32_Para/plots/Men_women.jpg)
-![Events](2021/w32_Para/plots/events.jpg)
-## Week 48 - Dr.Who [(code)](https://github.com/pgomba/TidyTuesday/tree/main/2021/w48_drWho)
-![Dr.Who](2021/w48_drWho/plots/drwho.jpg)
-# 2023
-## Week 5 - Pet Cats UK [(code)](https://github.com/pgomba/TidyTuesday/tree/main/2023/w5_cats)
-![cats](2023/w5_cats/cats_uk.png)
+
+## 2024
+
+### Week 50 - The scent of data (code)
+
+![](2024/w_50/perfumes.png)
+
+## 2023
+
+### Week 5 - Pet Cats UK [(code)](https://github.com/pgomba/TidyTuesday/tree/main/2023/w5_cats) ![cats](2023/w5_cats/cats_uk.png){alt="cats"}
+
+## 2021
+
+### Week 31 - Olympic Medals [(code)](https://github.com/pgomba/TidyTuesday/tree/main/2021/w31_Olimp)
+
+### ![Olimpic](2021/w31_Olimp/plots/spain_medals.jpeg) other countries plots [(here)](https://github.com/pgomba/TidyTuesday/tree/main/2021/w31_Olimp/plots) \## Week 32 - Paralympic Medals [(code)](https://github.com/pgomba/TidyTuesday/tree/main/2021/w32_Para) ![MvsW](2021/w32_Para/plots/Men_women.jpg) ![Events](2021/w32_Para/plots/events.jpg) Week 48 - Dr.Who [(code)](https://github.com/pgomba/TidyTuesday/tree/main/2021/w48_drWho) ![Dr.Who](2021/w48_drWho/plots/drwho.jpg) 
